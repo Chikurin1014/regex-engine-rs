c0bf78e2994e60f81166a6e9877595da415b0c73
Chikurin1014 <chikurin1014@gmail.com>
2024-11-15 15:29:40 +0900

    :sparkles: ユーザーAPIの実装

--- src/engine.rs
+++ src/engine.rs
@@ -1,5 +1,7 @@
 use std::fmt::Display;
 
+use crate::helper::DynError;
+
 pub mod codegen;
 pub mod evaluator;
 pub mod parser;
@@ -21,4 +23,37 @@ impl Display for Instruction {
             Instruction::Split(addr1, addr2) => write!(f, "split {:>04} {:>04}", addr1, addr2),
         }
     }
+}
+
+/// 正規表現と文字列をマッチングする
+///
+/// # 利用例
+///
+/// ```
+/// use regex_engine;
+/// regex_engine::do_matching(
+///     expr: "abc|(de|cd)+",
+///     line: "decddede",
+///     is_depth: true
+/// );
+/// ```
+///
+/// # 引数
+///
+/// - expr: &str - 正規表現
+/// - line: &str - マッチング対象の文字列
+/// - is_depth: bool - 深さ優先探索を行うかどうか
+///
+/// # 戻り値
+///
+/// エラーなく実行でき、かつマッチングに**成功**した場合はOk(true)を返し、
+/// エラーなく実行できたがマッチングに**失敗**した場合はOk(false)を返す。
+///
+/// 入力された正規表現や内部的な実装エラーがあった場合はErrを返す。
+///
+pub fn do_matching(expr: &str, line: &str, is_depth: bool) -> Result<bool, DynError> {
+    let ast = parser::parse(expr)?;
+    let code = codegen::gen_code(&ast)?;
+    let line = line.chars().collect::<Vec<_>>();
+    Ok(evaluator::eval(&code, &line, is_depth)?)
 }
